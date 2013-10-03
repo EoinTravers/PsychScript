19194a6e3e2fbf73fd5364755b8ca7f1e2fed546
Eoin Travers <eoint02@gmail.com>
2013-10-03 12:14:53 +0100

    Created templated for mouse tracking

--- mouse-tracking/js/functions.js
+++ mouse-tracking/js/functions.js
@@ -1,205 +1,217 @@
-// Setup Functions
-function preload_images(image_names, directory) {
-	var image_array = new Array();
-	for (var i = 0; i < image_names.length; i++) {
-		var img = new Image()
-		img.src = directory + image_names[i]
-		image_array.push(img)
-	};
-	return image_array;
-};
+/*
+    This page is part of PsychScript
+    Copyright (C) 2013 Eoin Travers etravers01@qub.ac.uk.
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-// Canvas functions and classes
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-// Classes
-// Text
-var Text = function(text, scale, y) {
-	this.text = text || 'No text given';
-	this.scale = scale || 1;
-	this.y = y || canvas.height * .5;
-};
-Text.prototype.draw = function() {
-	draw_text(this.text, canvas.width*.5, this.y, font_size*this.scale);
-};
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-//Images (generic)
-var Icon = function(label, x, y, img_url) {
-	this.x = x || 0;
-	this.y = y || 0;
-	this.w = pic_w;
-	this.h = pic_h;
-	this.label = label;
-	this.image = new Image();
-	//this.image.src = 'images/' + img_url || 'images/no.png'; // should just use the url given
-	this.image.src = img_url;
-}
-Icon.prototype.draw = function() {
-	ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
-}
-Icon.prototype.contains = function(mx, my) {
-	return	(this.x <= mx) && (this.x + this.w >= mx) &&
-		(this.y <= my) && (this.y + this.h >= my);
-};
+/*
+ * This page consists of generic functions, that should be useful in a 
+ * range of paradigms. As such, we would hope to shield the end user from
+ * the actual code here, and just provide some idea of what functions should
+ * be called in the main.js section of their own experiment.
+*/
 
-// Response images
-var Response = function(label, x, y, img_url) {
-	this.x = x || 0;
-	this.y = y || 0;
-	this.w = pic_w;
-	this.h = pic_h;
-	this.label = label;
-	this.image = new Image();
-	this.image.src = img_url;
-}
-Response.prototype = Object.create(Icon.prototype)
-Response.prototype.onclick = function(){
-};
+// Don't change anything below this line.
+// Trial variables
+var trial_number = 0;
+var stimuli_number;
+// Running variables - Defined here so the user doesn't have to deal with them.
+var start_time;
+var response;
+var rt;
+var prime;
+var probe;
+var accepted_keys
 
-// Button image
-var Button = function(img_url) {
-	this.h = h*.05;
-	this.w = this.h * 2;
-	this.x = (w*.5)-(this.w*.5); // relative to canvas.width?
-	this.y = h-this.h;
-	this.image = new Image();
-	this.image.src = img_url;
-	this.label = img_url.slice(0,-4);
-	this.action = function(){pass()};// This function will change throughout the trial
-};
-Button.prototype = Object.create(Icon.prototype)
-Button.prototype.onclick = function(){ // Dynamically change this.action() to modify function of the button.
-	this.action();
-};
 
 // Functions
-var draw_me = new Array();
-function update_screen(){
-	draw_blank();
-	for(var i = 0; i < draw_me.length; i++){
-		draw_me[i].draw();
-	};
+// Visuals
+function show_text(text) {
+	document.getElementById('probeText').innerHTML = text;
+};
+function show_above(text) {
+	document.getElementById('textAboveProbe').innerHTML = text;
+};
+function show_below(text) {
+	document.getElementById('textBelowProbe').innerHTML = text;
+};
+function show_image(locationID, src) {
+	var this_image = document.getElementById(locationID)
+	this_image.src = '../images/' + src;
+	this_image.style.display = 'Block';
+};
+function hide_images(){
+	document.getElementById('img1').style.display = 'None';
+	document.getElementById('img2').style.display = 'None';
+	document.getElementById('img3').style.display = 'None';
 };
 
-function draw_text(text_input, x, y, font_size, color) {
-	// Splits text across multiple lines, using '\n'.
-	this.text_input = this.text_input || 'No text given';
-	ctx.textAlign = 'center';
-	ctx.font= String(font_size)+"px Arial"
-	ctx.fillStyle= color || 'black';
-	text_lines = text_input.split('\n');
-	height = y
-	for(var i = 0; i < text_lines.length; i ++) {
-		ctx.fillText(text_lines[i], x, height);
-		height = height + font_size
-	};
-};
-
-function draw_blank() {
-	ctx.fillStyle="GhostWhite";
-	ctx.fillRect(0,0,w,h)
-};
-
-function draw_responses() {
-	resp1.draw(ctx);
-	resp2.draw(ctx);
-	button.draw(ctx);
-};
-
-function draw_button() {
-	button.draw(ctx);
-};
-
-function plot_last(){ // Plots the mouse data from the last trial.
-	for (var i=0;i<xList.length;i++)
-	{
-		ctx.fillStyle="red";
-		ctx.fillRect(xList[i], yList[i], 10, 10);
-	};
-};
-
-function pass() {
-	console.log('pass')
-};
-
-// Mouse handling
-document.onmousemove = function(e){
-    mx = e.pageX - canvas.offsetLeft
-    my = e.pageY - canvas.offsetTop
-    //$('#x').html(mx); // For debugging - Disable for speed
-	//$('#y').html(my); // For debugging
-};
-canvas.onmousedown = function(e){
-    mx = e.pageX - canvas.offsetLeft
-    my = e.pageY - canvas.offsetTop
-    //onsole.log(String(mx)+' '+String(my))
-    for(var i = 0; i < clickables.length; i++){
-		if(clickables[i].contains(mx, my)){
-			clickables[i].onclick()
-		};
+function show_fixation(duration, flash) {
+    flash = flash || false
+    if (flash) {
+    	var pause = duration / 3
+	show_text('')
+	setTimeout("show_text('+');", pause)
+	setTimeout("show_text('');", pause*2)
+    }
+    else {
+	show_text('+');
     };
 };
 
-function track_mouse() { //N.B Declare var start_time = Date.now() immediately before running.
-	t = Date.now() - start_time;
-	xList.push(mx)
-	yList.push(my)
-	tList.push(t) // Instantiate faster?
-};
 
-function python_debug() {
-    document.getElementById('py_x').innerHTML = xList;
-    document.getElementById('py_y').innerHTML = yList;
-    document.getElementById('py_t').innerHTML = tList;
-};
-
-function mousetrack_fixation(next_function, duration) {
-	/* Flashes a fixation cross on and off for duration ms, and
-	 * then executes next_function (usually the mouse tracking phase).*/
-	var pause = duration / 3
-	prime_text.text = '';
-	draw_me = Array(resp1, resp2, prime_text);
-	update_screen();
-    timer1 = setTimeout(function(){
-		prime_text.text = '+';
-		update_screen();
-		}, pause);
-	timer2 = setTimeout(function(){
-		prime_text.text = '';
-		update_screen();
-		}, pause*2);
-	timer3 = setTimeout(function(){
-		clearInterval(homeInterval);
-		next_function()}, duration);
-	homeInterval = setInterval(function(){check_home(next_function, duration)}, 50);
-};
-
-function check_home(next_function, duration){
-	if(button.contains(mx, my) == false){
-		// Stop the scheduled stimuli
-		clearInterval(homeInterval);
-		clearTimeout(timer1);
-		clearTimeout(timer2);
-		clearTimeout(timer3);
-		alert(early_start_msg);
-		mousetrack_fixation(next_function, duration);
+// Response handling
+function get_keyboard_response(keys_to_accept) {
+    accepted_keys = keys_to_accept
+    document.onkeydown = function(event) {
+	var key = String.fromCharCode(event.keyCode)
+	if(accepted_keys == undefined) {
+	    response = key
+	    log_response()
+	}
+	else {
+	    var key
+	    if (accepted_keys.indexOf(key) != -1) {
+		response = key
+		log_response()
+	    }
+	    else if (accepted_keys.indexOf(key.toLowerCase()) != -1) {
+		// In case acccepted_keys were given in lowercase.
+		response = key
+		log_response()
+	    };
 	};
+    };
 };
 
-//~ var ctx
-//~ // Create the canvas
-//~ function set_up_canvas(w, h)  {
-	//~ // Caution: This will return a canvas object, but not the context.
-	//~ // Use as follows:
-	//~ //	var canvas = set_up_canvas(200, 150);
-	//~ //	var ctx = canvas.getContext("2d");
-	//~ var canvas = document.getElementById('canvas1')
-	//~ canvas.style.marginLeft = "auto";
-	//~ canvas.style.marginRight = "auto";
-	//~ canvas.style.display = "Block";
-	//~ canvas.width = w;
-	//~ canvas.height = h;
-	//~ //canvas.offsetLeft = window.innerWidth *.25; // is this right?
-	//~ ctx = canvas.getContext("2d");
-	//~ return(canvas)
-//~ };
+function activate_response_buttons(list_of_button_ids) {
+    for (var i=0; i < list_of_button_ids.length; i++)  {
+	var button = document.getElementById(list_of_button_ids[i]);
+	console.log(i+button)
+	button.onclick = function(){
+	    response = this.innerHTML
+	    log_response(response);
+	};
+    };
+};
+
+function deactivate_response_buttons(list_of_button_ids) {
+    for (var i=0; i < list_of_button_ids.length; i++)  {
+	var button_id = list_of_button_ids[i]
+	document.getElementById(button_id).onclick = function(){};
+    };
+};
+
+
+// Experiment logic
+function begin_experiment() {
+	document.getElementById('instructions').style.display = "None";
+	document.getElementById('experiment').style.display = "Inline";
+	trial_stage0(trial_number)
+};
+
+function end_experiment() {
+	document.getElementById('experiment').style.display = "None";
+	document.getElementById('debrief').style.display = "Inline";
+};
+
+
+// Logging
+function check_accuracy(condition, response, conditions, correct_responses) {
+    var condition_index = conditions.indexOf(condition);
+    var correct_response = correct_responses[condition_index]
+    if (response == correct_response) {
+	return true
+    }
+    else {
+	return false
+    };
+};
+
+function log_response() {
+    rt = Date.now() - start_time;
+    // If they exit, clear intervals and timeouts.
+    try {clearInterval(tracking_interval);} catch(err){};
+    try {clearTimeout(response_timeout);} catch(err){};
+    console.log('Logging response: '+ response)
+    for (var i=0; i < variables_to_log.length; i++) {
+	var logging_box_id = logging_box_ids[i];
+	var variable_to_log = variables_to_log[i];
+	console.log(variable_to_log)
+	document.getElementById(logging_box_id).value = window[variable_to_log]
+    };
+    //sendData(data_address);
+    next_trial();
+};
+
+function next_trial(){
+    if (trial_number < probes.length-1) {
+	trial_number++
+	trial_stage0(trial_number)
+    }
+    else {
+	end_experiment();
+    };
+};
+
+function sendData(address){ // POSTs data in responseForm to the address given.
+    $.ajax({
+	    type: "POST",
+	    //url: "save_data.php",
+	    url: address,
+	    data: $("#responseForm").serialize(), // serializes the form's elements.
+	    success: function(data)
+	    {
+	       console.log('data sent')
+	    }
+    });
+};
+		
+// Other functions
+function shuffle(array){ //Shuffle an array
+    //+ Jonas Raoni Soares Silva
+    //@ http://jsfromhell.com/array/shuffle [v1.0]
+    for(var j, x, i = array.length; i; j = Math.floor(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
+    return array;
+};
+
+function range(start, end) { // Creates range of numbers from start to end (similar to Python)
+    var ar = [];
+    for (var i = start; i < end; i++) {
+        ar.push(i);
+    };
+    return ar;
+};
+
+function generate_random_list(length){
+    return shuffle(range(0, length));
+};
+
+if (!Array.prototype.indexOf) { // Compatibility fix for IE. See http://stackoverflow.com/questions/143847/best-way-to-find-an-item-in-a-javascript-array#144172
+    Array.prototype.indexOf = function (obj, fromIndex) {
+        if (fromIndex == null) {
+            fromIndex = 0;
+        } else if (fromIndex < 0) {
+            fromIndex = Math.max(0, this.length + fromIndex);
+        }
+        for (var i = fromIndex, j = this.length; i < j; i++) {
+            if (this[i] === obj)
+                return i;
+        }
+        return -1;
+    };
+}
+
+
--- mouse-tracking/js/main.js
+++ mouse-tracking/js/main.js
@@ -1,12 +1,5 @@
-/*
- * 
- * TO DO:
- * Deal with slow starts, timeouts, and mouse movements before probe onset (either prompt, or control).
- * Randomize response side
-*/
-
 // Experiment stuff
-var subject_nr = Math.floor(Math.random() * 99999999) + 1;
+// Parameters
 var sample_rate = 10;
 var max_init_time = 800;
 var max_response_time = 4000;
@@ -14,166 +7,149 @@ var early_start_msg = "Whoah, not so fast!\nPlease don't start moving until you 
 var late_start_msg = "Good, but remember, you're under time pressure.\n\nPlease try to start moving as soon\
  as you see the final word, even if you're not fully sure of your answer yet.";
 var timeout_msg = "Oh no! You ran out of time!\n\nPlease try to answer more quickly.";
-var timed_out = false
-var property_type
-var property_name
-var text_list
+var number_of_trials = probes.length
+
+// Variables to record
+var experiment_start_time = Date();
+var subject_nr = Math.floor(Math.random() * 99999999) + 1; // Pick a random number between 1 and 99999999
+var probe
+var code
+var response
+var rt
+var xList = new Array(); // Mouse tracking coordinates (x, y, and time)
+var yList = new Array();
+var tList = new Array();
+var trial_number // Trials in chronological order
+var stimuli_number // ID of the stimuli used in this trial
+var w // Canvas dimensions
+var h
+
+// List of variables to record
+var variables_to_log = new Array(
+	'experiment_start_time', 'subject_nr', 'probe', 'code', 'response', 'rt', 
+	'xList', 'yList', 'tList', 'trial_number', 'stimuli_number', 'w', 'h'
+	);
+// The HTML IDs of the text boxes where each variable will be recorded. (see #DOCUMENTATION#)
+// These MUST be in the same order as variables_to_log, above.
+var logging_box_ids = new Array(
+	"timeBox", "subjectBox", "probeBox", "codeBox", "responseBox", "rtBox", "xBox",
+	"yBox", "tBox", "trialBox", "stimuliBox", "widthBox", "heightBox"
+	);
 
 
-// Create canvas
-var canvas = document.getElementById('canvas1');
+//  Canvas settings
 canvas.height = window.innerHeight *.9;
-canvas.width = canvas.height*1.6; // 4:3 ration
-var ctx = canvas.getContext('2d');
-var w = canvas.width;
-var h = canvas.height;
-var font_size = Math.round(h/25) //  Make relative
-var pic_w = w * .2; // Again, looks ok.
+canvas.width = canvas.height*1.6; // Aspect ratio
+w = canvas.width;
+h = canvas.height;
+var font_size = Math.round(h/25) //  Size of text, relative to screen
+var pic_w = w * .2; // Default dimensions of icons.
 var pic_h = pic_w*.75;
+
+// Some important variables. Do not change.
+var trial_order = shuffle(range(0, number_of_trials));
+var timed_out = false
+var slow_start = false;
+var mx;
+var my;
+var draw_me = new Array(); // A list of things to draw when the screen is updated.
+
 
 /* Stimuli classes */
 
 // The icons we'll need
-var target = new Icon('', (w*.5)-(pic_w*.5), (h*.3), 'no.png');
-var resp1 = new Response('Yes', 10, 10, 'yes.png');
-var resp2 = new Response('No', canvas.width-pic_w-10, 10, 'no.png');
-var feedback = new Response('Feedback', (canvas.width*.5)-(pic_w*.5), (canvas.height*.5)-(pic_h*.5), 'no.png');
-resp1.image = button_imgs[0];
-resp2.image = button_imgs[1];
-var prime_text = new Text('Prime text');
+/*
+ * Here, we create the objects that participants interact with in the experiment,
+ * in based on some templates.
+ * An Icon is the most basic kind of image: it is pic_w wide, pic_h tall, and does nothing.
+ * Here, we create one Icon, called target, which can appear in the middle of the screen.
+ * Icons need 4 parameters: a label, the x coordinate of their left edge, the y coordinate
+ * of their top edge, and and URL of the image they show.
+ * Here, we label the Icon 'Target', place it half way across the screen (minus half it's width,
+ * so that it actually ends up in the middle), similarly center it vertically, and give it
+ * the image stored in 'images/no.png'.
+ * Although we won't be using this Icon in the Lexical Decision Task, we will use an identical one,
+ * called Feedback, to display a Cross when participants give a wrong answer.
+ * 
+ * We will also create our two Responses, resp1 and resp2.
+ * Responses work exactly like Icons, except that they can be made execute a function when clicked.
+ */ 
+var target = new Icon('Target', (w*.5)-(pic_w*.5), (h*.5), 'images/no.png');
+var feedback = new Icon('Feedback', (canvas.width*.5)-(pic_w*.5), (canvas.height*.5)-(pic_h*.5), 'images/no.png');
+var resp1 = new Response('Yes', 10, 10, 'images/yes.png');
+var resp2 = new Response('No', canvas.width-pic_w-10, 10, 'images/no.png');
+//resp1.image = button_imgs[0];
+//resp2.image = button_imgs[1];
+/* Another kind of object is Text.
+ * All we do now is create the objects, and give them their default text.
+ * Later, we can change this text, and show it onscreen.
+ * Finally, a Button is like an Icon, but smaller, and always shown in the
+ * bottom center of the screen.
+ */
+var prime_text = new Text('Click START to begin.'); 
+// Our prime in this case is just the fixation cross, but you must always create it.
 var probe_text = new Text('Probe text');
-var button = new Button('next.png');
+var button = new Button('images/next.png');
+// clickables is a list of objects which, when clicked on, will do something
+// (activate their 'action').
 var clickables = new Array(resp1, resp2, button);
 
 
 
 // Trial logic
-function trial_stage0() { // This will be run automatically once the page has loaded.
-	n = trial_order[trial];
-	console.log('Trial number: ' + String(trial) + '; Stimuli number: ' + String(n))
-	probe_text.text = probes[n];
-	prime_text.text = primes[n];
-	code = codes[n];
-	trial++;
+function trial_stage0() {
+	// Set up variables for the trial to come
+	stimuli_number = trial_order[trial_number];
+	console.log('Trial number: ' + String(trial_number) + '; Stimuli number: ' + String(stimuli_number))
+	probe = probes[stimuli_number];
+	probe_text.text = probe
+	code = codes[stimuli_number];
+	trial_number++; // Increase trial_number by one, to keep track.
+	// Clear the mouse data.
 	xList = [];
 	yList = [];
 	tList = [];
+	// Proceed to the next stage.
 	trial_stage1();
 };
 
 function trial_stage1(){
-	//console.log('Stage 1')
 	button.image.src = 'images/start.png'
 	draw_me = Array(resp1, resp2, prime_text, button);
+	// Set the button to, when clicked, show a fixation for 1500ms, then
+	// run trial stage 2.
 	button.action = function(){mousetrack_fixation(trial_stage2, 1500)}
 	update_screen();
+	// Nothing will now happen until the button is clicked.
 };
 
 function trial_stage2() {
-	clearInterval(homeInterval);
-	draw_me = Array(resp1, resp2, probe_text);
+	clearInterval(homeInterval); // DO NOT REMOVE
+	// While the fixation is showing, moving the mouse will reset the trial
+	// to the start. clearInterval prevents this.
+	draw_me = Array(resp1, resp2, probe_text); // Show the probe_text.
 	update_screen();
+	// Make the response buttons do something.
 	resp1.onclick = function(){
-		log_response(1)};
+		response = 1
+		log_response()};
 	resp2.onclick = function(){
-		log_response(2)};
+		response = 2
+		log_response()};
+	// Start the timer - ALWAYS DO THIS
 	start_time = Date.now()
+	// Schedule the mouse to be recorded every 'sample_rate' ms (defined avove)
 	tracking_interval = setInterval(function(){track_mouse();}, sample_rate);
+	// Set the trial to time out after 'max_response_time' ms (again, defined above)
+	// Note, you must set some timeout, although you can make it as long as you wish.
 	response_timeout = setTimeout(function(){responseTimeout();}, max_response_time);
 };
 
-function check_if_late(maximum_init_time) {
-	this.maximum_init_time = this.maximum_init_time || max_init_time // Revert to global
-	var time_to_check = this.maximum_init_time/sample_rate
-	return button.contains(xList[time_to_check], yList[time_to_check])
-};
-
-function responseTimeout() {
+function responseTimeout() { // Consider moving to functions?
+	// What to do if trial times out.
+	// By default, shows timeout_msg in a popup window, and records a response of -1
 	alert(timeout_msg);
-	timed_out = true;
-	log_response(-1);
+	//timed_out = true;
+	response = -1
+	log_response();
 };
-	
-	
-// Variables not to change, unless you know what you're doing.
-var x;
-var y;
-var trial = 0;
-var trial_order = shuffle(range(0, 48)); // only use first 24 trials.
-var response_1;
-var response_2;
-var target;
-var slow_start = false;
-var response;
-var n; 
-xList = new Array();
-yList = new Array();
-tList = new Array();
-// Canvas, etc
-//var h = window.innerHeight * .9; // Canvas is 90% of screen height
-//var w = h * 1.333; // Aspect ration 4:3
-//var canvas = set_up_canvas(w, h);
-//document.body.appendChild(canvas);
-//var font_size = Math.round(h/30);
-
-
-
-// Canvas functions
-function log_response(resp) {
-	clearInterval(tracking_interval);
-	clearTimeout(response_timeout);
-	//python_debug(); // For testing only
-	//plot_last();
-	var correct = true
-	console.log(resp)
-	if (code == 'aw') {
-		if (resp == 2 ) {
-			 correct = false
-		 };
-	}
-	else {
-		if (resp == 1 ) {
-			 correct = false
-		 };
-	 };
-	if (!correct){
-		draw_me = Array(resp1, resp2, feedback);
-	}
-	else {
-		draw_me = Array(resp1, resp2);
-	};
-	update_screen();
-	if (check_if_late() &! timed_out){
-		alert(late_start_msg)
-	};
-	timed_out = false
-	rt = tList[tList.length-1];
-	document.getElementById('timeBox').value = Date();
-	document.getElementById('subjectBox').value = subject_nr;
-	document.getElementById('trialBox').value = trial;
-	document.getElementById('nBox').value = n;
-	document.getElementById('responseBox').value = resp;
-	document.getElementById('rtBox').value = rt;
-	document.getElementById('widthBox').value = canvas.width;
-	document.getElementById('heightBox').value = canvas.height;
-	document.getElementById('xBox').value = xList;
-	document.getElementById('yBox').value = yList;
-	document.getElementById('tBox').value = tList;
-	sendData("save_data.php");
-	if (trial < trial_order.length){
-			//console.log('Again!')
-			click = false;
-			//delay here?
-			setTimeout(function(){trial_stage0()}, 500);
-		}
-	else{
-		document.getElementById("endText").style.display = "Block";
-		document.getElementById("canvas1").style.display = "None";
-		canvas.height = 2;
-		//end()
-		//alert('End of demo')
-	};
-};
-
-
-
