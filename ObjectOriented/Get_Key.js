238f304df85451ec4c5152c6330183195f4d9fe9
Eoin Travers <eoint02@gmail.com>
2013-10-09 12:07:58 +0100

    OO changes

--- ObjectOriented/Get_Key.js
+++ ObjectOriented/Get_Key.js
@@ -8,30 +8,35 @@ function Get_Key(keys_to_accept) {
 	};
 	window['response'] = undefined;
 	document.onkeydown = function(event) {
+		console.log('key down')
 		var key = String.fromCharCode(event.keyCode)
 		var response_time = Date.now()
 		var accepted_keys = window['keys_to_accept']
+		console.log(accepted_keys)
 		if (window['accept_all_keys']) {
+			console.log(key)
 			window['response'] = key;
 			window['response_time'] = response_time;
 			//return {response: key, response_time: response_time};
 		}
 		else {
 			if (accepted_keys.indexOf(key) != -1) {
+				console.log(key)
 				window['response'] = key;
 				window['response_time'] = response_time;
 				//return {response: key, response_time: response_time};
 			};
 		};
 	};
-	this.check_for_response();
+	return check_for_response();
 };
 
-Get_Key.prototype.check_for_response = function(){
+var check_for_response = function(){
 	if (!window['response']) {
-		setTimeout(this.check_for_response(), 100)
+		setTimeout("check_for_response()", 100)
 	}
 	else {
-		return response
+		alert(window['response'])
+		return true
 	};
 };
