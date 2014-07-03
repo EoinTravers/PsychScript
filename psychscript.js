936f0ebbb8f5c44118b9ee03df1c7ea765450b7e
Eoin Travers <eoint02@gmail.com>
2014-07-03 15:58:41 +0100

    Big cleanup, and commenting, on main .js file

--- psychscript.js
+++ psychscript.js
@@ -23,11 +23,15 @@
  * be called in the main.js section of their own experiment.
 */
 
-// Don't change anything below this line.
-// Trial variables
+
+// Running variables - Defined here so the user doesn't have to deal with them.
+/* Ideally, the user would define these themselves, and maybe it would
+ * be better practice, but for now they're defined here, so that the experiment
+ * will still work.
+ */
+
 var trial_number = 0;
 var stimuli_number;
-// Running variables - Defined here so the user doesn't have to deal with them.
 var start_time;
 var response;
 var rt;
@@ -38,27 +42,53 @@ var feedback_delay;
 
 // Functions
 // Visuals
+
+/* These are really just shortcuts - short oneliners to
+ * replace longer, more complex oneliners, but at the expensive of
+ * flexability.
+ * 
+ * TO DO - Document how users can create their own visual functions
+ */
+ 
+function hide_element(id){
+    document.getElementById(id).style.display = 'None';
+}
+function show_element(id){
+    document.getElementById(id).style.display = 'Block';
+    //Should this be inline?
+}
+
 function show_text(text) {
-	document.getElementById('probeText').innerHTML = text;
+    document.getElementById('probeText').innerHTML = text;
 };
 function show_above(text) {
-	document.getElementById('textAboveProbe').innerHTML = text;
+    document.getElementById('textAboveProbe').innerHTML = text;
 };
 function show_below(text) {
-	document.getElementById('textBelowProbe').innerHTML = text;
+    document.getElementById('textBelowProbe').innerHTML = text;
 };
+
+
 function show_image(locationID, src) {
-	var this_image = document.getElementById(locationID)
-	this_image.src = '../images/' + src;
-	this_image.style.display = 'Block';
+    var this_image = document.getElementById(locationID)
+    this_image.src = '../images/' + src;
+    this_image.style.display = 'Block';
 };
 function hide_images(){
-	document.getElementById('img1').style.display = 'None';
-	document.getElementById('img2').style.display = 'None';
-	document.getElementById('img3').style.display = 'None';
+    // This was written for a specific experiment - needs to be made general purpose
+    document.getElementById('img1').style.display = 'None';
+    document.getElementById('img2').style.display = 'None';
+    document.getElementById('img3').style.display = 'None';
 };
-function show_fixation(duration, flash) {
-    flash = flash || false
+function show_fixation(duration, flash, id) {
+    /*Shows a fixation cross in element `id` for the given duration.
+     * Dy default, shows for the whole duration, but can optionally
+     * show blank, fixation, and blank again, for 1/3 of the duartion each.
+     * If `id` is not given, it defaults to `probeText`, which should
+     * be fine for most cases.
+     */
+    var flash = flash || false;
+    var id = id || 'probeText';
     if (flash) {
     	var pause = duration / 3
 		show_text('')
@@ -72,6 +102,11 @@ function show_fixation(duration, flash) {
 
 // Response handling
 function get_keyboard_response(keys_to_accept) {
+    /* 'Turn on' the keyboard, so that, on a keystroke,
+     * the experiment will check if the key is one of the ones
+     * expected (if any are supplied, otherwise it will accept any key),
+     * and log that key as the response.
+     */
     accepted_keys = keys_to_accept
     document.onkeydown = function(event) {
 	var key = String.fromCharCode(event.keyCode)
@@ -97,10 +132,14 @@ function get_keyboard_response(keys_to_accept) {
 };
 
 function deactivate_keyboard_response(){
-	document.onkeydown = function(){}
+    // Self explanatory really
+    document.onkeydown = function(){}
 };
 
 function activate_response_buttons(list_of_button_ids) {
+    // 'Turn on' all of the buttons id'd, so that on being
+    // clicked, they call `log_response` with their value as
+    // `response`.
     for (var i=0; i < list_of_button_ids.length; i++)  {
 	var button = document.getElementById(list_of_button_ids[i]);
 	console.log(i+button)
@@ -112,19 +151,21 @@ function activate_response_buttons(list_of_button_ids) {
 };
 
 function deactivate_response_buttons(list_of_button_ids) {
+    // Does what it says.
     for (var i=0; i < list_of_button_ids.length; i++)  {
 	var button_id = list_of_button_ids[i]
 	document.getElementById(button_id).onclick = function(){};
     };
 };
 
-
 // Experiment logic
 function begin_experiment() {
-	create_logging_form("responseForm", variables_to_log)
-	document.getElementById('instructions').style.display = "None";
-	document.getElementById('experiment').style.display = "Inline";
-	trial_stage0()
+    // Users can of course override these functions, but these are
+    // useful defaults.
+    create_logging_form(variables_to_log); // See function below   
+    document.getElementById('instructions').style.display = "None"; // Hide instructions
+    document.getElementById('experiment').style.display = "Inline"; // Show exp. body.
+    trial_stage0() // Run first trial.
 };
 
 function end_experiment() {
@@ -134,6 +175,91 @@ function end_experiment() {
 
 
 // Logging
+function create_logging_form(variables_list, formID) {
+    /*
+     * This takes a list of variables, and creates, within `formID`, 
+     * a text box for each, id'd `variablenameBox`, and named `variablenameOut`
+     * (this name is used by the logging script on the server).
+     * After each trial, each variable in `variables_list` will be written
+     * to it's appropriate box, and the form will be submitted to the server
+     * via AJAX, by `log_response()`.
+     */
+    var formID = formID || 'responseForm'; // This shouldn't need to be changed. 
+    var form = document.getElementById(formID);
+    for(var i=0; i < variables_list.length; i++){
+	var variable = variables_list[i]
+	var label = document.createTextNode(variable);
+	var inp = document.createElement("input");
+	inp.setAttribute('id', variable+'Box');
+	inp.setAttribute('name', variable+'Out');
+	var line = document.createElement('br');
+	form.appendChild(label);
+	form.appendChild(inp);
+	form.appendChild(line);
+    };
+};
+
+
+
+function log_response() {
+    rt = Date.now() - start_time;
+    // If they exit, clear intervals and timeouts.
+    try {clearInterval(tracking_interval);} catch(err){};
+    try {clearTimeout(response_timeout);} catch(err){};
+    feedback_delay = 0 // Leave at 0 until implemented
+    if(error_feedback) {
+	// Not fully implemented yet
+	var correct = check_accuracy()
+	console.log(correct)
+	if (!correct) {
+	    give_feedback()
+	};
+    };
+    console.log('Logging response: '+ response)
+    for (var i=0; i < variables_to_log.length; i++) {
+	// Loop through `variables_to_log`, and write each
+	// variable to the approriate text box.
+	var variable_to_log = variables_to_log[i];
+	var logging_box_id = variable_to_log + 'Box';
+	console.log(logging_box_id)
+	document.getElementById(logging_box_id).value = window[variable_to_log]
+	// `window['variable'] is the only way I know of using a string of
+	// a variables name in place of the actual variable.
+    };
+    sendData(data_address);
+    setTimeout(function(){next_trial();}, feedback_delay)
+};
+
+
+
+function next_trial(){
+    // Start next trial, if `trial_number` hasn't exceeded `number_of_trials`.
+    // Bad things will happen if neither variable is defined.
+    // `trial_number` is a built-in variable - no other label should be used.
+    if (trial_number < number_of_trials) {
+	trial_number++ 
+	trial_stage0()
+    }
+    else {
+	end_experiment();
+    };
+};
+
+function sendData(address){ // POSTs data in responseForm to the address given.
+    $.ajax({
+	    type: "POST",
+	    url: address, // I'm not sure this actually does anything,
+	    // as the address is hard-coded in the html of `responseForm`.
+	    data: $("#responseForm").serialize(), // serializes the form's elements.
+	    success: function(data)
+	    {
+	       console.log('data sent')
+	    }
+    });
+};
+		
+// The next two functions aren't fully implemented yet,
+// but can be utilized at the user's own risk.
 function check_accuracy() {
     var codes_index = list_of_codes.indexOf(code);
     var correct_response = correct_responses[codes_index]
@@ -149,60 +275,16 @@ function check_accuracy() {
     };
 };
 
-function log_response() {
-    rt = Date.now() - start_time;
-    // If they exit, clear intervals and timeouts.
-    try {clearInterval(tracking_interval);} catch(err){};
-    try {clearTimeout(response_timeout);} catch(err){};
-    feedback_delay = 0
-    if(error_feedback) {
-	var correct = check_accuracy()
-	console.log(correct)
-	if (!correct) {
-	    give_feedback()
-	};
-    };
-    console.log('Logging response: '+ response)
-    for (var i=0; i < variables_to_log.length; i++) {
-	var variable_to_log = variables_to_log[i];
-	var logging_box_id = variable_to_log + 'Box';
-	console.log(logging_box_id)
-	document.getElementById(logging_box_id).value = window[variable_to_log]
-    };
-    sendData(data_address);
-    setTimeout(function(){next_trial();}, feedback_delay)
-};
-
 function give_feedback() {
     console.log('Error: Blank feedback used')
     // This is the default function, which does nothing.
     // Override this to give feedback
 };
-
-function next_trial(){
-    if (trial_number < probes.length-1) {
-	trial_number++
-	trial_stage0(trial_number)
-    }
-    else {
-	end_experiment();
-    };
-};
-
-function sendData(address){ // POSTs data in responseForm to the address given.
-    $.ajax({
-	    type: "POST",
-	    //url: "save_data.php",
-	    url: address,
-	    data: $("#responseForm").serialize(), // serializes the form's elements.
-	    success: function(data)
-	    {
-	       console.log('data sent')
-	    }
-    });
-};
 		
-// Other functions
+//////////////////////////
+// Miscellaneous	//
+//////////////////////////
+
 function shuffle(array){ //Shuffle an array
     //+ Jonas Raoni Soares Silva
     //@ http://jsfromhell.com/array/shuffle [v1.0]
@@ -210,7 +292,8 @@ function shuffle(array){ //Shuffle an array
     return array;
 };
 
-function range(start, end) { // Creates range of numbers from start to end (similar to Python)
+function range(start, end) { 
+    // Creates range of numbers from start to end (similar to Python)
     var ar = [];
     for (var i = start; i < end; i++) {
         ar.push(i);
@@ -222,7 +305,8 @@ function generate_random_list(length){
     return shuffle(range(0, length));
 };
 
-if (!Array.prototype.indexOf) { // Compatibility fix for IE. See http://stackoverflow.com/questions/143847/best-way-to-find-an-item-in-a-javascript-array#144172
+if (!Array.prototype.indexOf) { 
+    // Compatibility fix for IE. See http://stackoverflow.com/questions/143847/best-way-to-find-an-item-in-a-javascript-array#144172
     Array.prototype.indexOf = function (obj, fromIndex) {
         if (fromIndex == null) {
             fromIndex = 0;
@@ -238,26 +322,16 @@ if (!Array.prototype.indexOf) { // Compatibility fix for IE. See http://stackove
 }
 
 function preload_images(image_names, directory) {
-	var image_array = new Array();
-	for (var i = 0; i < image_names.length; i++) {
-		var img = new Image()
-		img.src = directory + image_names[i]
-		image_array.push(img)
-	};
-	return image_array;
+    // Can be used to load image files prior to start of experiment,
+    // rather than for each trial.
+    // This probably isn't the best way to achieve this though.
+    var image_array = new Array();
+    for (var i = 0; i < image_names.length; i++) {
+	    var img = new Image()
+	    img.src = directory + image_names[i]
+	    image_array.push(img)
+    };
+    return image_array;
 };
 
-function create_logging_form(formID, variables_list) {
-    var form = document.getElementById(formID);
-    for(var i=0; i < variables_list.length; i++){
-	var variable = variables_list[i]
-	var label = document.createTextNode(variable);
-	var inp = document.createElement("input");
-	inp.setAttribute('id', variable+'Box');
-	inp.setAttribute('name', variable+'Out');
-	var line = document.createElement('br');
-	form.appendChild(label);
-	form.appendChild(inp);
-	form.appendChild(line);
-    };
-};
+
